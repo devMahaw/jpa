8eddaa6c281eefc76849e0bf578ca7c3fd863edd
MateusADantas <mahawprogramacao@gmail.com>
2024-09-28 17:09:06 -0300

    Classe MatriculaDao Change

--- src/main/java/br/com/mdantas/dao/MatriculaDao.java
+++ src/main/java/br/com/mdantas/dao/MatriculaDao.java
@@ -31,6 +31,29 @@ public class MatriculaDao implements IMatriculaDao {
     }
 
     @Override
+    public Matricula buscarPorCodigoCurso(String codigoCurso) {
+        EntityManagerFactory entityManagerFactory =
+                Persistence.createEntityManagerFactory("ExemploJPA");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("SELECT m FROM Matricula m ");
+        sb.append("INNER JOIN Curso c on c = m.curso ");
+        sb.append("WHERE c.codigo = :codigoCurso");
+
+        entityManager.getTransaction().begin();
+        TypedQuery<Matricula> query =
+                entityManager.createQuery(sb.toString(), Matricula.class);
+        query.setParameter("codigoCurso", codigoCurso);
+        Matricula matricula = query.getSingleResult();
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+        return matricula;
+    }
+
+    @Override
     public Matricula buscarPorCurso(Curso curso) {
         EntityManagerFactory entityManagerFactory =
                 Persistence.createEntityManagerFactory("ExemploJPA");
